606fc2491e045a91c92f1f3158a35fa9601e5fa0
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-05-08 22:08:44 +0900

    implement unit test for the targeted bug.

---
+++ src/slnStartupProjectUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("slnStartupProjectUnitTest")]
+[assembly: AssemblyDescription("A .NET library to set the default StartUp project of a Visual Studio solution file")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("iVoltage")]
+[assembly: AssemblyProduct("sln StartUp Project UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Michel Courtine 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2003a5e1-c1f2-4998-9cd3-0674220fce56")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
---
+++ src/slnStartupProjectUnitTest/UnitTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using NUnit.Framework;
+using slnStartupProjectLibrary;
+
+namespace slnStartupUnitTest
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        readonly Assembly assemby = Assembly.GetAssembly(typeof(UnitTest));
+        private string testSolutionDirectory;
+
+        /// <summary>
+        /// SetUp
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            testSolutionDirectory = GetTestSolutionDirectory();
+        }
+
+        /// <summary>
+        /// TearDown
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        /// <summary>
+        /// Run Test
+        /// </summary>
+        [Test]
+        public void Run()
+        {
+            var baseSolution = Path.Combine(testSolutionDirectory, "ExampleProject.sln");
+            var processedSolution = Path.Combine(testSolutionDirectory, "ExampleProjectProcessed.sln");
+            var tempSolution = Path.Combine(testSolutionDirectory, "ExampleProjectTemp.sln");
+
+            File.Copy(baseSolution, tempSolution);
+            Parser.SetStartupProject(tempSolution, "ExampleProject");
+
+            FileAssert.AreEqual(processedSolution, tempSolution);
+
+            File.Delete(tempSolution);
+        }
+
+        /// <summary>
+        /// Get the Directory of the Assemby
+        /// </summary>
+        /// <returns></returns>
+        private string GetAssembyDirectory()
+        {
+            var assembyPath = this.assemby.Location;
+            var assembyDirecctory = Path.GetDirectoryName(assembyPath);
+            return assembyDirecctory;
+        }
+
+        /// <summary>
+        /// Get the Directory of test solution
+        /// </summary>
+        /// <returns></returns>
+        private string GetTestSolutionDirectory()
+        {
+            var testSolutionDirecotry = Path.GetFullPath(Path.Combine(GetAssembyDirectory(), @"..\..\..\..\..\tests\data\ExampleProject"));
+            return testSolutionDirecotry;
+        }
+    }
+}
