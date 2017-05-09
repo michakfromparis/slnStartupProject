2104aa0f67208d20fcf65e0a0aa9e608900c4f0a
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-05-09 21:32:55 +0900

    implement a test when a solution has BOM.

--- src/slnStartupProjectUnitTest/UnitTest.cs
+++ src/slnStartupProjectUnitTest/UnitTest.cs
@@ -33,13 +33,36 @@ namespace slnStartupUnitTest
         }
 
         /// <summary>
-        /// Run Test
+        /// Run Test when the solution has dependencies and doesn't have BOM.
         /// </summary>
         [Test]
         public void Run()
         {
             var baseSolution = Path.Combine(testSolutionDirectory, "ExampleProject.sln");
             var processedSolution = Path.Combine(testSolutionDirectory, "ExampleProjectProcessed.sln");
+            var tempSolution = Path.Combine(testSolutionDirectory, "ExampleProjectTemp.sln");
+
+            if (File.Exists(tempSolution))
+            {
+                File.Delete(tempSolution);
+            }
+            File.Copy(baseSolution, tempSolution);
+            Parser.SetStartupProject(tempSolution, "ExampleProject");
+
+            FileAssert.AreEqual(processedSolution, tempSolution);
+
+            File.Delete(tempSolution);
+        }
+
+
+        /// <summary>
+        /// Run Test when the solution has dependencies and has BOM.
+        /// </summary>
+        [Test]
+        public void RunWithBOM()
+        {
+            var baseSolution = Path.Combine(testSolutionDirectory, "ExampleProjectBOM.sln");
+            var processedSolution = Path.Combine(testSolutionDirectory, "ExampleProjectProcessedBOM.sln");
             var tempSolution = Path.Combine(testSolutionDirectory, "ExampleProjectTemp.sln");
 
             if (File.Exists(tempSolution))
