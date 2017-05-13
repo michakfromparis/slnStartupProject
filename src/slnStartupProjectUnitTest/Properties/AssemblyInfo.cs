7b1718eb2b35b884f2373fa4468875a6d1390ba5
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-05-13 10:57:09 +0900

    add a unit test for the bug that a solution file corrupt when it has dependencies.

---
+++ src/slnStartupProjectUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("slnStartupProjectUnitTest")]
+[assembly: AssemblyDescription("A .NET library to set the default StartUp project of a Visual Studio solution file")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("iVoltage")]
+[assembly: AssemblyProduct("sln StartUp Project UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Michel Courtine 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bbe2a7b3-5a0f-4326-ad58-bd6d3f7b956c")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
---
+++ src/slnStartupProjectUnitTest/UnitTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using NUnit.Framework;
+using slnStartupProjectLibrary;
+
+namespace slnStartupUnitTest
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        readonly Assembly assemby = Assembly.GetAssembly(typeof(UnitTest));
+        private string testSolutionDirectory;
+
+        /// <summary>
+        /// SetUp
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            testSolutionDirectory = GetTestSolutionDirectory();
+        }
+
+        /// <summary>
+        /// TearDown
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        /// <summary>
+        /// Run Test when the solution has dependencies and doesn't have BOM.
+        /// </summary>
+        [Test]
+        public void Run()
+        {
+            var baseSolution = Path.Combine(testSolutionDirectory, "ExampleProject.sln");
+            var processedSolution = Path.Combine(testSolutionDirectory, "ExampleProjectProcessed.sln");
+            var tempSolution = Path.Combine(testSolutionDirectory, "ExampleProjectTemp.sln");
+
+            if (File.Exists(tempSolution))
+            {
+                File.Delete(tempSolution);
+            }
+            File.Copy(baseSolution, tempSolution);
+            Parser.SetStartupProject(tempSolution, "ExampleProject");
+
+            FileAssert.AreEqual(processedSolution, tempSolution);
+
+            File.Delete(tempSolution);
+        }
+
+
+        /// <summary>
+        /// Run Test when the solution has dependencies and has BOM.
+        /// </summary>
+        [Test]
+        public void RunWithBOM()
+        {
+            var baseSolution = Path.Combine(testSolutionDirectory, "ExampleProjectBOM.sln");
+            var processedSolution = Path.Combine(testSolutionDirectory, "ExampleProjectProcessedBOM.sln");
+            var tempSolution = Path.Combine(testSolutionDirectory, "ExampleProjectTemp.sln");
+
+            if (File.Exists(tempSolution))
+            {
+                File.Delete(tempSolution);
+            }
+            File.Copy(baseSolution, tempSolution);
+            Parser.SetStartupProject(tempSolution, "ExampleProject");
+
+            FileAssert.AreEqual(processedSolution, tempSolution);
+
+            File.Delete(tempSolution);
+        }
+
+        /// <summary>
+        /// Get the Directory of the Assemby
+        /// </summary>
+        /// <returns></returns>
+        private string GetAssembyDirectory()
+        {
+            var assembyPath = this.assemby.Location;
+            var assembyDirecctory = Path.GetDirectoryName(assembyPath);
+            return assembyDirecctory;
+        }
+
+        /// <summary>
+        /// Get the Directory of test solution
+        /// </summary>
+        /// <returns></returns>
+        private string GetTestSolutionDirectory()
+        {
+            var testSolutionDirecotry = Path.GetFullPath(Path.Combine(GetAssembyDirectory(), @"..\..\tests\data\ExampleProject"));
+            return testSolutionDirecotry;
+        }
+    }
+}
